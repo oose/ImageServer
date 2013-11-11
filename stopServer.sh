46999e9d446cd2214b1c24b5b94a39405e97556e
Markus Klink <justjoheinz@gmail.com>
2013-11-11 14:16:38 +0100

    startServer.sh

--- stopServer.sh
+++ stopServer.sh
@@ -1,4 +1,10 @@
-figlet ImageServer
+
+if hash figlet 2>/dev/null; then
+    figlet ImageServer
+else
+    echo -e ImageServer
+fi
+
 echo -e "\033[1mStopping servers\033[0m"
 kill  `cat PID9001.pid` > /dev/null
 kill  `cat PID9002.pid` > /dev/null
